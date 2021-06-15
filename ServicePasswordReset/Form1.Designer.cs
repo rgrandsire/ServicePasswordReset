9b88fb52dc12c3e86dc8ce5ac0ad72df32d6555f
rgrandsire@gmail.com <598Th0mas>
2021-06-15 17:12:14 -0400

    First draft

--- ServicePasswordReset/Form1.Designer.cs
+++ ServicePasswordReset/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 174);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(361, 174);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Connect and Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(492, 198);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HighQA Service user password factory reset";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ServicePasswordReset/Form1.cs
+++ ServicePasswordReset/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,37 @@ namespace ServicePasswordReset
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\temp\\ResetServicePassword.cmd"))
+            {
+                try
+                {
+                    File.Delete("C:\\temp\\ResetServicePassword.cmd");
+                }
+                catch { }
+            }
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = {"@echo off","title Getting User status","rem Creating the sql file",
+                              "echo UPDATE dbo.Core_UserAccounts set CustomPassword='4xQvZNBTuO9dZGcWJPqFkw==' where CustomUser='Service'; > temp.sql",
+                              "echo Running the command", "sqlcmd -S localhost\\highqa17 -d HQA_CDB -U hqaservice5 -P WRD3!4x6K8CmfL#c@rBA -o Results.txt -i temp.sql",
+                              "del temp.sql", "@exit"};
+            //check if C:\temp exist
+            if (!Directory.Exists("C:\\temp")) Directory.CreateDirectory("C:\\temp)");
+            File.WriteAllLines("C:\\temp\\ResetServicePassword.cmd", lines);
+            var startInfo = new ProcessStartInfo("C:\\temp\\ResetServicePassword.cmd");
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.LoadUserProfile = true;
+            Process.Start(startInfo);
+            MessageBox.Show("Successfully reset the Service password", "Password Resewt Status", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
     }
 }
+
